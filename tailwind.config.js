317b8f8c722466c8e28953474ddde8e664871d02
Tom Thornton <tom@launchpeer.com>
2019-10-11 13:42:26 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                "white": "white",
+                "dark": "#121212",
+                "dark-100": "#1E1E1E",
+                "dark-200": "#222222",
+                "dark-300": "#252525",
+                "dark-400": "#272727",
+                "dark-500": "#2C2C2C",
+                "dark-600": "#2D2D2D",
+                "dark-700": "#333333",
+                "dark-800": "#363636",
+                "dark-900": "#383838"
+            }
+        },
+        margin: {
+            sm: "8px",
+            md: "16px",
+            lg: "24px",
+            xl: "48px",
+        },
+        padding: {
+            sm: "8px",
+            md: "16px",
+            lg: "24px",
+            xl: "48px",
+        }
+    },
+    variants: {},
+    plugins: [],
+}
